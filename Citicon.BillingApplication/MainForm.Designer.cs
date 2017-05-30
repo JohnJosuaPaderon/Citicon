9124a49df3e03b829e02ec12f49b4d79fed1085e
johnpad53 <johnjosua.paderon@outlook.com>
2017-05-30 19:06:33 +0800

    Updated App.config Updated MainForm.cs Updated MainForm.Designer.cs

--- Citicon.BillingApplication/MainForm.Designer.cs
+++ Citicon.BillingApplication/MainForm.Designer.cs
@@ -30,56 +30,106 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.DeliveryConfirmationButton = new System.Windows.Forms.Button();
-            this.EngineerButton = new System.Windows.Forms.Button();
+            this.PurchaseOrderButton = new System.Windows.Forms.Button();
+            this.PaymentButton = new System.Windows.Forms.Button();
+            this.SalesInvoiceButton = new System.Windows.Forms.Button();
+            this.BillingButton = new System.Windows.Forms.Button();
+            this.UserSettingsButton = new System.Windows.Forms.Button();
             this.TallySheetButton = new System.Windows.Forms.Button();
-            this.DeliveryButton = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.AutoSize = true;
-            this.panel1.Controls.Add(this.DeliveryConfirmationButton);
-            this.panel1.Controls.Add(this.EngineerButton);
             this.panel1.Controls.Add(this.TallySheetButton);
-            this.panel1.Controls.Add(this.DeliveryButton);
+            this.panel1.Controls.Add(this.UserSettingsButton);
+            this.panel1.Controls.Add(this.PurchaseOrderButton);
+            this.panel1.Controls.Add(this.PaymentButton);
+            this.panel1.Controls.Add(this.SalesInvoiceButton);
+            this.panel1.Controls.Add(this.BillingButton);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
             this.panel1.Padding = new System.Windows.Forms.Padding(0, 5, 0, 5);
-            this.panel1.Size = new System.Drawing.Size(786, 60);
+            this.panel1.Size = new System.Drawing.Size(1472, 60);
             this.panel1.TabIndex = 1;
             // 
-            // DeliveryConfirmationButton
+            // PurchaseOrderButton
             // 
-            this.DeliveryConfirmationButton.BackColor = System.Drawing.Color.Red;
-            this.DeliveryConfirmationButton.FlatAppearance.BorderSize = 0;
-            this.DeliveryConfirmationButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.DeliveryConfirmationButton.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.DeliveryConfirmationButton.ForeColor = System.Drawing.Color.White;
-            this.DeliveryConfirmationButton.Location = new System.Drawing.Point(414, 5);
-            this.DeliveryConfirmationButton.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
-            this.DeliveryConfirmationButton.Name = "DeliveryConfirmationButton";
-            this.DeliveryConfirmationButton.Size = new System.Drawing.Size(150, 50);
-            this.DeliveryConfirmationButton.TabIndex = 3;
-            this.DeliveryConfirmationButton.Text = "CONFIRM DELIVERY";
-            this.DeliveryConfirmationButton.UseVisualStyleBackColor = false;
+            this.PurchaseOrderButton.BackColor = System.Drawing.Color.Red;
+            this.PurchaseOrderButton.FlatAppearance.BorderSize = 0;
+            this.PurchaseOrderButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.PurchaseOrderButton.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PurchaseOrderButton.ForeColor = System.Drawing.Color.White;
+            this.PurchaseOrderButton.Location = new System.Drawing.Point(364, 5);
+            this.PurchaseOrderButton.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.PurchaseOrderButton.Name = "PurchaseOrderButton";
+            this.PurchaseOrderButton.Size = new System.Drawing.Size(150, 50);
+            this.PurchaseOrderButton.TabIndex = 3;
+            this.PurchaseOrderButton.Text = "PURCHASE ORDER";
+            this.PurchaseOrderButton.UseVisualStyleBackColor = false;
             // 
-            // EngineerButton
+            // PaymentButton
             // 
-            this.EngineerButton.BackColor = System.Drawing.Color.Red;
-            this.EngineerButton.FlatAppearance.BorderSize = 0;
-            this.EngineerButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.EngineerButton.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.EngineerButton.ForeColor = System.Drawing.Color.White;
-            this.EngineerButton.Location = new System.Drawing.Point(279, 5);
-            this.EngineerButton.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
-            this.EngineerButton.Name = "EngineerButton";
-            this.EngineerButton.Size = new System.Drawing.Size(125, 50);
-            this.EngineerButton.TabIndex = 2;
-            this.EngineerButton.Text = "CREATE DESIGN";
-            this.EngineerButton.UseVisualStyleBackColor = false;
+            this.PaymentButton.BackColor = System.Drawing.Color.Red;
+            this.PaymentButton.FlatAppearance.BorderSize = 0;
+            this.PaymentButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.PaymentButton.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PaymentButton.ForeColor = System.Drawing.Color.White;
+            this.PaymentButton.Location = new System.Drawing.Point(254, 5);
+            this.PaymentButton.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.PaymentButton.Name = "PaymentButton";
+            this.PaymentButton.Size = new System.Drawing.Size(100, 50);
+            this.PaymentButton.TabIndex = 2;
+            this.PaymentButton.Text = "PAYMENT";
+            this.PaymentButton.UseVisualStyleBackColor = false;
+            // 
+            // SalesInvoiceButton
+            // 
+            this.SalesInvoiceButton.BackColor = System.Drawing.Color.Red;
+            this.SalesInvoiceButton.FlatAppearance.BorderSize = 0;
+            this.SalesInvoiceButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SalesInvoiceButton.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SalesInvoiceButton.ForeColor = System.Drawing.Color.White;
+            this.SalesInvoiceButton.Location = new System.Drawing.Point(119, 5);
+            this.SalesInvoiceButton.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.SalesInvoiceButton.Name = "SalesInvoiceButton";
+            this.SalesInvoiceButton.Size = new System.Drawing.Size(125, 50);
+            this.SalesInvoiceButton.TabIndex = 1;
+            this.SalesInvoiceButton.Text = "SALES INVOICE";
+            this.SalesInvoiceButton.UseVisualStyleBackColor = false;
+            // 
+            // BillingButton
+            // 
+            this.BillingButton.BackColor = System.Drawing.Color.Red;
+            this.BillingButton.FlatAppearance.BorderSize = 0;
+            this.BillingButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BillingButton.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BillingButton.ForeColor = System.Drawing.Color.White;
+            this.BillingButton.Location = new System.Drawing.Point(9, 5);
+            this.BillingButton.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.BillingButton.Name = "BillingButton";
+            this.BillingButton.Size = new System.Drawing.Size(100, 50);
+            this.BillingButton.TabIndex = 0;
+            this.BillingButton.Text = "BILLING";
+            this.BillingButton.UseVisualStyleBackColor = false;
+            this.BillingButton.Click += new System.EventHandler(this.BillingButton_Click);
+            // 
+            // UserSettingsButton
+            // 
+            this.UserSettingsButton.BackColor = System.Drawing.Color.Red;
+            this.UserSettingsButton.FlatAppearance.BorderSize = 0;
+            this.UserSettingsButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.UserSettingsButton.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UserSettingsButton.ForeColor = System.Drawing.Color.White;
+            this.UserSettingsButton.Location = new System.Drawing.Point(659, 5);
+            this.UserSettingsButton.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.UserSettingsButton.Name = "UserSettingsButton";
+            this.UserSettingsButton.Size = new System.Drawing.Size(125, 50);
+            this.UserSettingsButton.TabIndex = 4;
+            this.UserSettingsButton.Text = "USER SETTINGS";
+            this.UserSettingsButton.UseVisualStyleBackColor = false;
             // 
             // TallySheetButton
             // 
@@ -88,35 +138,20 @@
             this.TallySheetButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.TallySheetButton.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.TallySheetButton.ForeColor = System.Drawing.Color.White;
-            this.TallySheetButton.Location = new System.Drawing.Point(144, 5);
+            this.TallySheetButton.Location = new System.Drawing.Point(524, 5);
             this.TallySheetButton.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.TallySheetButton.Name = "TallySheetButton";
             this.TallySheetButton.Size = new System.Drawing.Size(125, 50);
-            this.TallySheetButton.TabIndex = 1;
+            this.TallySheetButton.TabIndex = 5;
             this.TallySheetButton.Text = "TALLY SHEET";
             this.TallySheetButton.UseVisualStyleBackColor = false;
-            // 
-            // DeliveryButton
-            // 
-            this.DeliveryButton.BackColor = System.Drawing.Color.Red;
-            this.DeliveryButton.FlatAppearance.BorderSize = 0;
-            this.DeliveryButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.DeliveryButton.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.DeliveryButton.ForeColor = System.Drawing.Color.White;
-            this.DeliveryButton.Location = new System.Drawing.Point(9, 5);
-            this.DeliveryButton.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
-            this.DeliveryButton.Name = "DeliveryButton";
-            this.DeliveryButton.Size = new System.Drawing.Size(125, 50);
-            this.DeliveryButton.TabIndex = 0;
-            this.DeliveryButton.Text = "DELIVERY";
-            this.DeliveryButton.UseVisualStyleBackColor = false;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(786, 729);
+            this.ClientSize = new System.Drawing.Size(1472, 729);
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.Color.Red;
@@ -127,6 +162,7 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Citicon";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.MainForm_Load);
             this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -136,10 +172,12 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button DeliveryConfirmationButton;
-        private System.Windows.Forms.Button EngineerButton;
+        private System.Windows.Forms.Button PurchaseOrderButton;
+        private System.Windows.Forms.Button PaymentButton;
+        private System.Windows.Forms.Button SalesInvoiceButton;
+        private System.Windows.Forms.Button BillingButton;
+        private System.Windows.Forms.Button UserSettingsButton;
         private System.Windows.Forms.Button TallySheetButton;
-        private System.Windows.Forms.Button DeliveryButton;
     }
 }
 
--- Citicon.BillingApplication/MainForm.cs
+++ Citicon.BillingApplication/MainForm.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Citicon.Data;
+using Citicon.Forms;
+using System;
 using System.Windows.Forms;
 
 namespace Citicon.BillingApplication
@@ -15,6 +10,48 @@ namespace Citicon.BillingApplication
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private BillingForm BillingForm
+        {
+            get
+            {
+                var form = new BillingForm() { MdiParent = this };
+                form.FormClosed += (s, e) => form = null;
+                return form;
+            }
+        }
+
+        private void InitializeSecurity()
+        {
+            UserSettingsButton.Enabled = User.CurrentUser.Admin;
+        }
+
+        private void OpenMdiChild(Form form)
+        {
+            if (ActiveMdiChild != null && ActiveMdiChild.GetType() == form.GetType())
+            {
+                return;
+            }
+            else
+            {
+                if (ActiveMdiChild != null)
+                {
+                    ActiveMdiChild.Close();
+                }
+
+                form.Show();
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            InitializeSecurity();
+        }
+
+        private void BillingButton_Click(object sender, EventArgs e)
+        {
+            OpenMdiChild(BillingForm);
         }
     }
 }
