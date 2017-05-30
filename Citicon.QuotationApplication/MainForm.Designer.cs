6321cbf0e0d110c88f043dec7917d3a7e19a69a1
johnpad53 <johnjosua.paderon@outlook.com>
2017-05-30 13:51:02 +0800

    Updated Citicon.Quotation Project. Updated MainForm.cs Updated MainForm.Designer.cs

--- Citicon.QuotationApplication/MainForm.Designer.cs
+++ Citicon.QuotationApplication/MainForm.Designer.cs
@@ -29,27 +29,65 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.HomeButton = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Form1
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.Controls.Add(this.HomeButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(0, 5, 0, 5);
+            this.panel1.Size = new System.Drawing.Size(863, 60);
+            this.panel1.TabIndex = 1;
+            // 
+            // HomeButton
+            // 
+            this.HomeButton.BackColor = System.Drawing.Color.Red;
+            this.HomeButton.FlatAppearance.BorderSize = 0;
+            this.HomeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.HomeButton.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HomeButton.ForeColor = System.Drawing.Color.White;
+            this.HomeButton.Location = new System.Drawing.Point(9, 5);
+            this.HomeButton.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.HomeButton.Name = "HomeButton";
+            this.HomeButton.Size = new System.Drawing.Size(125, 50);
+            this.HomeButton.TabIndex = 0;
+            this.HomeButton.Text = "HOME";
+            this.HomeButton.UseVisualStyleBackColor = false;
+            this.HomeButton.Click += new System.EventHandler(this.HomeButton_Click);
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(331, 341);
+            this.ClientSize = new System.Drawing.Size(863, 806);
+            this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.Color.Red;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.IsMdiContainer = true;
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.Name = "Form1";
+            this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Citicon";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button HomeButton;
     }
 }
 
--- Citicon.QuotationApplication/MainForm.cs
+++ Citicon.QuotationApplication/MainForm.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Citicon.Forms;
+using System;
 using System.Windows.Forms;
 
 namespace Citicon.QuotationApplication
@@ -15,6 +9,43 @@ namespace Citicon.QuotationApplication
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        public ClientManagementForm ClientManagementForm
+        {
+            get
+            {
+                var form = new ClientManagementForm() { MdiParent = this };
+                form.FormClosed += (s, e) => form = null;
+                return form;
+            }
+        }
+
+        private void OpenMdiChild(Form form)
+        {
+            if (ActiveMdiChild != null && ActiveMdiChild.GetType() == form.GetType())
+            {
+                return;
+            }
+            else
+            {
+                if (ActiveMdiChild != null)
+                {
+                    ActiveMdiChild.Close();
+                }
+
+                form.Show();
+            }
+        }
+
+        private void HomeButton_Click(object sender, EventArgs e)
+        {
+            OpenMdiChild(ClientManagementForm);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            OpenMdiChild(ClientManagementForm);
         }
     }
 }
