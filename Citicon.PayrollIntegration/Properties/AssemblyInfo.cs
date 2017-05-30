ae010881936c3974de0e6912fb6345ee74a64603
johnpad53 <johnjosua.paderon@outlook.com>
2017-05-30 16:19:43 +0800

    Updated AssemblyInfo.cs

--- Citicon.PayrollIntegration/Properties/AssemblyInfo.cs
+++ Citicon.PayrollIntegration/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Citicon.PayrollIntegration")]
-[assembly: AssemblyCopyright("Copyright © John Josua R. Paderon 2017")]
+[assembly: AssemblyCopyright("Copyright © 2017 John Josua R. Paderon")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
