366cc743ab760cca1501ebfd9aab93ffe44c1228
johnpad53 <johnjosua.paderon@outlook.com>
2017-05-30 19:57:10 +0800

    Added BillingPaymentManager.cs

---
+++ Citicon/DataManager/BillingPaymentManager.cs
@@ -0,0 +1,17 @@
+﻿using Citicon.Data;
+using Citicon.DataProcess;
+using System.Threading.Tasks;
+
+namespace Citicon.DataManager
+{
+    public static class BillingPaymentManager
+    {
+        public static async Task SavePaymentAsync(BillingPayment billingPayment)
+        {
+            using (var savePayment = new SavePayment(billingPayment))
+            {
+                await savePayment.ExecuteAsync();
+            }
+        }
+    }
+}
