16b85a9fc97ad484d4bfdf8b2862baef60d7aff5
johnpad53 <johnjosua.paderon@outlook.com>
2017-06-03 15:28:55 +0800

    Added QuotationRevisionHistoryItem.cs

---
+++ Citicon/Data/QuotationRevisionHistoryItem.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Citicon.Data
+{
+    public class QuotationRevisionHistoryItem
+    {
+        public QuotationRevisionHistoryItem(Quotation quotation)
+        {
+            Quotation = quotation ?? throw new ArgumentNullException(nameof(quotation));
+            RevisionNumber = quotation.RevisionNumber + 1;
+        }
+
+        public Quotation Quotation { get; }
+        public uint RevisionNumber { get; }
+
+
+        public static bool operator ==(QuotationRevisionHistoryItem left, QuotationRevisionHistoryItem right)
+        {
+            if (ReferenceEquals(left, right))
+                return true;
+
+            if ((object)left == null || (object)right == null)
+                return false;
+
+            return left.Quotation == right.Quotation && left.RevisionNumber == right.RevisionNumber;
+        }
+
+        public static bool operator !=(QuotationRevisionHistoryItem left, QuotationRevisionHistoryItem right)
+        {
+            return !(left == right);
+        }
+
+        public override bool Equals(object arg)
+        {
+            if (arg is QuotationRevisionHistoryItem)
+            {
+                return (QuotationRevisionHistoryItem)arg == this;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return Quotation.GetHashCode() ^ RevisionNumber.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return RevisionNumber.ToString();
+        }
+    }
+}
