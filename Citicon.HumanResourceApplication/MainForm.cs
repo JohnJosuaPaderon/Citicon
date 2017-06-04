1e573f90ed59f020debebdc377b6c65d5c198384
johnpad53 <johnjosua.paderon@outlook.com>
2017-06-04 21:45:36 +0800

    Added App.config Added Citicon.HumanResourceApplication.csproj Added MainForm.cs Added MainForm.Designer.cs Added MainForm.resx Added Program.cs

---
+++ Citicon.HumanResourceApplication/MainForm.cs
@@ -0,0 +1,21 @@
+﻿using Citicon.Forms;
+using System;
+using System.Windows.Forms;
+
+namespace Citicon.HumanResourceApplication
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var form = new AddEditEmployeeForm();
+            form.ShowDialog();
+            form = null;
+        }
+    }
+}
