22891e884bb3917e924f742b2f51abe009287857
johnpad53 <johnjosua.paderon@outlook.com>
2017-06-04 19:15:59 +0800

    Added TextBoxField.cs Added TextBoxField.Designer.cs Added TextBoxField.resx

---
+++ Citicon/UI/Controls/TextBoxField.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace Citicon.UI.Controls
+{
+    public partial class TextBoxField : UserControl
+    {
+        public TextBoxField()
+        {
+            InitializeComponent();
+        }
+
+        public string Caption
+        {
+            get { return CaptionLabel.Text; }
+            set
+            {
+                if (CaptionLabel.Text == value) return;
+
+                CaptionLabel.Text = value;
+                CaptionLabel.Text = value;
+
+            }
+        }
+
+        [Browsable(true)]
+        public override string Text
+        {
+            get { return TextBox.Text; }
+            set
+            {
+                if (TextBox.Text == value) return;
+
+                TextBox.Text = value;
+                TextBox.Text = value;
+            }
+        }
+    }
+}
