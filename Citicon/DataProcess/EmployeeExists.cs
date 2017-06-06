8ed5d040f37618529fde03f7f9ddfccc6eb8171c
johnpad53 <johnjosua.paderon@outlook.com>
2017-06-06 14:02:28 +0800

    Added: EmployeeExists.cs Added: InsertEmployee.cs Added: UpdateEmployee.cs

---
+++ Citicon/DataProcess/EmployeeExists.cs
@@ -0,0 +1,42 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Threading.Tasks;
+
+namespace Citicon.DataProcess
+{
+    internal sealed class EmployeeExists : DataProcessBase
+    {
+        public EmployeeExists(string firstName, string middleName, string lastName)
+        {
+            FirstName = firstName;
+            MiddleName = middleName;
+            LastName = lastName;
+        }
+
+        private string FirstName;
+        private string MiddleName;
+        private string LastName;
+        private const string Parameter_FirstName = "@_FirstName";
+        private const string Parameter_MiddleName = "@_MiddleName";
+        private const string Parameter_LastName = "@_LastName";
+        
+        private MySqlCommand CreateCommand(MySqlConnection connection)
+        {
+            var command = new MySqlCommand()
+            {
+                CommandText = $"SELECT EmployeeExists({Parameter_FirstName}, {Parameter_MiddleName}, {Parameter_LastName});",
+                Connection = connection
+            };
+            command.Parameters.AddWithValue(Parameter_FirstName, FirstName);
+            command.Parameters.AddWithValue(Parameter_MiddleName, MiddleName);
+            command.Parameters.AddWithValue(Parameter_LastName, LastName);
+
+            return command;
+        }
+
+        public Task<bool> ExecuteAsync()
+        {
+            return ProcessUtility.HandleExecuteScalarAsync(CreateCommand, Convert.ToBoolean);
+        }
+    }
+}
---
+++ Citicon/DataProcess/UpdateEmployee.cs
@@ -0,0 +1,88 @@
+﻿using Citicon.Data;
+using MySql.Data.MySqlClient;
+using System;
+using System.Threading.Tasks;
+
+namespace Citicon.DataProcess
+{
+    internal sealed class UpdateEmployee : DataProcessBase
+    {
+        public UpdateEmployee(Employee employee)
+        {
+            Employee = employee ?? throw new ArgumentNullException(nameof(employee));
+        }
+
+        private Employee Employee;
+
+        private MySqlCommand CreateCommand(MySqlConnection connection, MySqlTransaction transaction)
+        {
+            var command = Utility.CreateProcedureCommand("UpdateEmployee", connection, transaction);
+            command.Parameters.AddWithValue("@_Id", Employee.Id);
+            command.Parameters.AddWithValue("@_FirstName", Employee.FirstName);
+            command.Parameters.AddWithValue("@_MiddleName", Employee.MiddleName);
+            command.Parameters.AddWithValue("@_LastName", Employee.LastName);
+            command.Parameters.AddWithValue("@_PositionId", Employee.Position?.Id);
+            command.Parameters.AddWithValue("@_BranchId", Employee.Branch?.Id);
+            command.Parameters.AddWithValue("@_CompanyId", Employee.Company?.Id);
+            command.Parameters.AddWithValue("@_PayrollType", Employee.PayrollType.ToString());
+            command.Parameters.AddWithValue("@_BirthDate", Employee.BirthDate);
+            command.Parameters.AddWithValue("@_CivilStatus", Employee.CivilStatus.ToString());
+            command.Parameters.AddWithValue("@_EmploymentDate", Employee.EmploymentDate);
+            command.Parameters.AddWithValue("@_TIN", Employee.TIN);
+            command.Parameters.AddWithValue("@_Address", Employee.Address);
+            command.Parameters.AddWithValue("@_TelephoneNumber", Employee.TelephoneNumber);
+            command.Parameters.AddWithValue("@_MobileNumber", Employee.MobileNumber);
+            command.Parameters.AddWithValue("@_EmailAddress", Employee.EmailAddress);
+            command.Parameters.AddWithValue("@_EmergencyContactPerson", Employee.EmergencyContactPerson);
+            command.Parameters.AddWithValue("@_EmergencyContactPersonNumber", Employee.EmergencyContactPersonNumber);
+            command.Parameters.AddWithValue("@_EmploymentStatus", Employee.EmploymentStatus.ToString());
+            command.Parameters.AddWithValue("@_SSSNumber", Employee.SSSNumber);
+            command.Parameters.AddWithValue("@_R1ADate", Employee.R1ADate);
+            command.Parameters.AddWithValue("@_PagIbigNumber", Employee.PagIbigNumber);
+            command.Parameters.AddWithValue("@_PhilHealthNumber", Employee.PhilHealthNumber);
+            command.Parameters.AddWithValue("@_PhilHealthRegistrationDate", Employee.PhilHealthRegistrationDate);
+            command.Parameters.AddWithValue("@_PagIbigRTN", Employee.PagIbigRTN);
+            command.Parameters.AddWithValue("@_PagIbigRegistrationDate", Employee.PagIbigRegistrationDate);
+            command.Parameters.AddWithValue("@_PagIbigRTNDependentNumber", Employee.PagIbigRTNDependentNumber);
+            command.Parameters.AddWithValue("@_VacationLeave", Employee.VacationLeave);
+            command.Parameters.AddWithValue("@_SickLeave", Employee.SickLeave);
+            command.Parameters.AddWithValue("@_Absences", Employee.Absences);
+            command.Parameters.AddWithValue("@_BasicPay", Employee.BasicPay);
+            command.Parameters.AddWithValue("@_DailyRate", Employee.DailyRate);
+            command.Parameters.AddWithValue("@_Allowance", Employee.Allowance);
+            command.Parameters.AddWithValue("@_OTAllowance", Employee.OTAllowance);
+            command.Parameters.AddWithValue("@_SSSDeduction", Employee.SSSDeduction);
+            command.Parameters.AddWithValue("@_SSSERDeduction", Employee.SSSERDeduction);
+            command.Parameters.AddWithValue("@_SSSECDeduction", Employee.SSSECDeduction);
+            command.Parameters.AddWithValue("@_WithHoldingTax", Employee.WithHoldingTax);
+            command.Parameters.AddWithValue("@_PagIbigDeduction", Employee.PagIbigDeduction);
+            command.Parameters.AddWithValue("@_PhilHealthDeduction", Employee.PhilHealthDeduction);
+
+            return command;
+        }
+
+        public override void Dispose()
+        {
+            Employee = null;
+            base.Dispose();
+        }
+
+        public async Task<Employee> ExecuteAsync(MySqlConnection connection, MySqlTransaction transaction)
+        {
+            using (var command = CreateCommand(connection, transaction))
+            {
+                if (await command.ExecuteNonQueryAsync() != 1)
+                {
+                    Employee = null;
+                }
+
+                return Employee;
+            }
+        }
+
+        public Task<Employee> ExecuteAsync()
+        {
+            return ProcessUtility.HandleExecuteAsync(ExecuteAsync);
+        }
+    }
+}
