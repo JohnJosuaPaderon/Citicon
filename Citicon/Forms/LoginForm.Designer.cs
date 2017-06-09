1459605d904f0ffd5a336bbb0c32c4c0f96d3889
johnpad53 <johnjosua.paderon@outlook.com>
2017-06-09 10:30:57 +0800

    Updated: LoginForm.Designer.cs

--- Citicon/Forms/LoginForm.Designer.cs
+++ Citicon/Forms/LoginForm.Designer.cs
@@ -44,10 +44,10 @@
             this.btnCancel.BackColor = System.Drawing.Color.White;
             this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnCancel.ForeColor = System.Drawing.Color.Red;
-            this.btnCancel.Location = new System.Drawing.Point(264, 306);
-            this.btnCancel.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnCancel.Location = new System.Drawing.Point(205, 248);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(113, 50);
+            this.btnCancel.Size = new System.Drawing.Size(88, 40);
             this.btnCancel.TabIndex = 15;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = false;
@@ -59,10 +59,10 @@
             this.btnLogin.BackColor = System.Drawing.Color.Red;
             this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnLogin.ForeColor = System.Drawing.Color.White;
-            this.btnLogin.Location = new System.Drawing.Point(146, 306);
-            this.btnLogin.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnLogin.Location = new System.Drawing.Point(114, 248);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(113, 50);
+            this.btnLogin.Size = new System.Drawing.Size(88, 40);
             this.btnLogin.TabIndex = 14;
             this.btnLogin.Text = "Login";
             this.btnLogin.UseVisualStyleBackColor = false;
@@ -71,10 +71,10 @@
             // tbxPassword
             // 
             this.tbxPassword.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.tbxPassword.Location = new System.Drawing.Point(20, 223);
-            this.tbxPassword.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.tbxPassword.Location = new System.Drawing.Point(16, 181);
+            this.tbxPassword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbxPassword.Name = "tbxPassword";
-            this.tbxPassword.Size = new System.Drawing.Size(357, 29);
+            this.tbxPassword.Size = new System.Drawing.Size(279, 25);
             this.tbxPassword.TabIndex = 13;
             this.tbxPassword.UseSystemPasswordChar = true;
             this.tbxPassword.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBox_Login);
@@ -83,20 +83,20 @@
             // 
             this.lblPassword.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lblPassword.AutoSize = true;
-            this.lblPassword.Location = new System.Drawing.Point(17, 200);
+            this.lblPassword.Location = new System.Drawing.Point(13, 162);
             this.lblPassword.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblPassword.Name = "lblPassword";
-            this.lblPassword.Size = new System.Drawing.Size(76, 21);
+            this.lblPassword.Size = new System.Drawing.Size(64, 17);
             this.lblPassword.TabIndex = 12;
             this.lblPassword.Text = "Password";
             // 
             // tbxUsername
             // 
             this.tbxUsername.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.tbxUsername.Location = new System.Drawing.Point(20, 160);
-            this.tbxUsername.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.tbxUsername.Location = new System.Drawing.Point(16, 130);
+            this.tbxUsername.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbxUsername.Name = "tbxUsername";
-            this.tbxUsername.Size = new System.Drawing.Size(357, 29);
+            this.tbxUsername.Size = new System.Drawing.Size(279, 25);
             this.tbxUsername.TabIndex = 11;
             this.tbxUsername.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBox_Login);
             // 
@@ -104,10 +104,10 @@
             // 
             this.lblUsername.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lblUsername.AutoSize = true;
-            this.lblUsername.Location = new System.Drawing.Point(17, 137);
+            this.lblUsername.Location = new System.Drawing.Point(13, 111);
             this.lblUsername.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblUsername.Name = "lblUsername";
-            this.lblUsername.Size = new System.Drawing.Size(81, 21);
+            this.lblUsername.Size = new System.Drawing.Size(67, 17);
             this.lblUsername.TabIndex = 10;
             this.lblUsername.Text = "Username";
             // 
@@ -116,18 +116,19 @@
             this.lblLogin.Dock = System.Windows.Forms.DockStyle.Top;
             this.lblLogin.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblLogin.Location = new System.Drawing.Point(0, 0);
+            this.lblLogin.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblLogin.Name = "lblLogin";
-            this.lblLogin.Size = new System.Drawing.Size(398, 84);
+            this.lblLogin.Size = new System.Drawing.Size(310, 68);
             this.lblLogin.TabIndex = 9;
             this.lblLogin.Text = "Login";
             this.lblLogin.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // LoginForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(398, 375);
+            this.ClientSize = new System.Drawing.Size(310, 304);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.tbxPassword);
@@ -135,11 +136,11 @@
             this.Controls.Add(this.tbxUsername);
             this.Controls.Add(this.lblUsername);
             this.Controls.Add(this.lblLogin);
-            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.Color.Red;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "LoginForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Citicon";
