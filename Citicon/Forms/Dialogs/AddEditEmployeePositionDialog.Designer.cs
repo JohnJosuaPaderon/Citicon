1493418e8d5836545b29ffc2325806a593cc222c
johnpad53 <johnjosua.paderon@outlook.com>
2017-06-24 08:28:36 +0800

    Added: AddEditEmployeePositionDialog.cs; AddEditEmployeePositionDIalog.Designer.cs; AddEditEmployeePositionDialog.resx

---
+++ Citicon/Forms/Dialogs/AddEditEmployeePositionDialog.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Citicon.Forms.Dialogs
+{
+    partial class AddEditEmployeePositionDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddEditEmployeePositionDialog));
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.DepartmentLabel = new System.Windows.Forms.Label();
+            this.DepartmentComboBox = new System.Windows.Forms.ComboBox();
+            this.CancelDialogButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Location = new System.Drawing.Point(12, 9);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(43, 17);
+            this.NameLabel.TabIndex = 0;
+            this.NameLabel.Text = "Name";
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.NameTextBox.Location = new System.Drawing.Point(15, 29);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(421, 25);
+            this.NameTextBox.TabIndex = 1;
+            // 
+            // DepartmentLabel
+            // 
+            this.DepartmentLabel.AutoSize = true;
+            this.DepartmentLabel.Location = new System.Drawing.Point(12, 57);
+            this.DepartmentLabel.Name = "DepartmentLabel";
+            this.DepartmentLabel.Size = new System.Drawing.Size(77, 17);
+            this.DepartmentLabel.TabIndex = 2;
+            this.DepartmentLabel.Text = "Department";
+            // 
+            // DepartmentComboBox
+            // 
+            this.DepartmentComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.DepartmentComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.DepartmentComboBox.FormattingEnabled = true;
+            this.DepartmentComboBox.Location = new System.Drawing.Point(15, 77);
+            this.DepartmentComboBox.Name = "DepartmentComboBox";
+            this.DepartmentComboBox.Size = new System.Drawing.Size(421, 25);
+            this.DepartmentComboBox.TabIndex = 3;
+            // 
+            // CancelDialogButton
+            // 
+            this.CancelDialogButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.CancelDialogButton.BackColor = System.Drawing.Color.White;
+            this.CancelDialogButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CancelDialogButton.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CancelDialogButton.ForeColor = System.Drawing.Color.Red;
+            this.CancelDialogButton.Location = new System.Drawing.Point(335, 116);
+            this.CancelDialogButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.CancelDialogButton.Name = "CancelDialogButton";
+            this.CancelDialogButton.Size = new System.Drawing.Size(100, 35);
+            this.CancelDialogButton.TabIndex = 125;
+            this.CancelDialogButton.Text = "CANCEL";
+            this.CancelDialogButton.UseVisualStyleBackColor = false;
+            this.CancelDialogButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.SaveButton.BackColor = System.Drawing.Color.Red;
+            this.SaveButton.FlatAppearance.BorderSize = 0;
+            this.SaveButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SaveButton.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SaveButton.ForeColor = System.Drawing.Color.White;
+            this.SaveButton.Location = new System.Drawing.Point(227, 116);
+            this.SaveButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(100, 35);
+            this.SaveButton.TabIndex = 124;
+            this.SaveButton.Text = "SAVE";
+            this.SaveButton.UseVisualStyleBackColor = false;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // AddEditEmployeePositionDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(448, 163);
+            this.Controls.Add(this.CancelDialogButton);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.DepartmentComboBox);
+            this.Controls.Add(this.DepartmentLabel);
+            this.Controls.Add(this.NameTextBox);
+            this.Controls.Add(this.NameLabel);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Red;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "AddEditEmployeePositionDialog";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Add/Edit Position";
+            this.Load += new System.EventHandler(this.AddEditEmployeePositionDialog_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.Label DepartmentLabel;
+        private System.Windows.Forms.ComboBox DepartmentComboBox;
+        private System.Windows.Forms.Button CancelDialogButton;
+        private System.Windows.Forms.Button SaveButton;
+    }
+}
---
+++ Citicon/Forms/Dialogs/AddEditEmployeePositionDialog.cs
@@ -0,0 +1,176 @@
+﻿using Citicon.Data;
+using Citicon.DataManager;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Citicon.Forms.Dialogs
+{
+    public partial class AddEditEmployeePositionDialog : Form
+    {
+        public static void AddEmployeePosition()
+        {
+            var dialog = new AddEditEmployeePositionDialog(new JobPosition(), DataDialogMode.Add);
+            dialog.ShowDialog();
+            dialog = null;
+        }
+
+        public static void EditEmployeePosition(JobPosition employeePosition)
+        {
+            var dialog = new AddEditEmployeePositionDialog(employeePosition, DataDialogMode.Edit);
+            dialog.ShowDialog();
+            dialog = null;
+        }
+
+        private JobPosition _EmployeePosition;
+        private DataDialogMode _Mode;
+
+        public JobPosition EmployeePosition
+        {
+            get { return _EmployeePosition; }
+            set
+            {
+                if (_EmployeePosition != value)
+                {
+                    _EmployeePosition = value;
+                    UpdateUI();
+                }
+            }
+        }
+
+        public DataDialogMode Mode
+        { 
+            get { return _Mode; }
+            set
+            {
+                if (_Mode != value)
+                {
+                    _Mode = value;
+                    UpdateMode();
+                }
+            }
+        }
+
+        private void UpdateMode()
+        {
+            switch (Mode)
+            {
+                case DataDialogMode.None:
+                    Text = "Nothing to do.";
+                    break;
+                case DataDialogMode.Add:
+                    Text = "Add new Position";
+                    break;
+                case DataDialogMode.Edit:
+                    Text = "Edit Position";
+                    break;
+            }
+        }
+
+        private void UpdateUI()
+        {
+            NameTextBox.Text = EmployeePosition.Name;
+            DepartmentComboBox.SelectedItem = EmployeePosition.Department;
+        }
+
+        private AddEditEmployeePositionDialog(JobPosition employeePosition, DataDialogMode mode)
+        {
+            InitializeComponent();
+            EmployeePosition = employeePosition ?? throw new ArgumentNullException(nameof(employeePosition));
+            Mode = mode;
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private async Task GetDepartmentListAsync()
+        {
+            DepartmentComboBox.Items.Clear();
+
+            try
+            {
+                var departments = await DepartmentManager.GetListAsync();
+
+                if (departments != null && departments.Any())
+                {
+                    DepartmentComboBox.Items.AddRange(departments.ToArray());
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private async void AddEditEmployeePositionDialog_Load(object sender, EventArgs e)
+        {
+            await GetDepartmentListAsync();
+            UpdateUI();
+        }
+
+        private async void SaveButton_Click(object sender, EventArgs e)
+        {
+            EmployeePosition.Name = NameTextBox.Text;
+            EmployeePosition.Department = DepartmentComboBox.SelectedItem as Department;
+            switch (Mode)
+            {
+                case DataDialogMode.None:
+                    MessageBox.Show("Nothing to be done.");
+                    break;
+                case DataDialogMode.Add:
+                    await InsertEmployeePositionAsync();
+                    break;
+                case DataDialogMode.Edit:
+                    await UpdateEmployeePositionAsync();
+                    break;
+            }
+        }
+
+        private async Task UpdateEmployeePositionAsync()
+        {
+            try
+            {
+                var employeePosition = await JobPositionManager.UpdateAsync(EmployeePosition);
+
+                if (employeePosition != null)
+                {
+                    MessageBox.Show("Successfully modified.");
+                    Close();
+                }
+                else
+                {
+                    MessageBox.Show("Failed to modify.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private async Task InsertEmployeePositionAsync()
+        {
+            try
+            {
+                var employeePosition = await JobPositionManager.InsertAsync(EmployeePosition);
+
+                if (employeePosition != null)
+                {
+                    MessageBox.Show("Successfully added.");
+                    Close();
+                }
+                else
+                {
+                    MessageBox.Show("Failed to add.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
