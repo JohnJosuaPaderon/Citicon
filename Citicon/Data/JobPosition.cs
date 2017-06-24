155a64f7a2955f72a4c8d56b9fbf83735e120031
johnpad53 <johnjosua.paderon@outlook.com>
2017-06-24 08:26:34 +0800

    Updated: JobPosition.cs

--- Citicon/Data/JobPosition.cs
+++ Citicon/Data/JobPosition.cs
@@ -6,6 +6,31 @@
         public string Name { get; set; }
         public Department Department { get; set; }
 
+        public static bool operator ==(JobPosition left, JobPosition right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(JobPosition left, JobPosition right)
+        {
+            return !(left == right);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (GetType() != obj.GetType()) return false;
+
+            var value = obj as JobPosition;
+            return Id.Equals(value.Id);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
         public override string ToString()
         {
             return Name;
