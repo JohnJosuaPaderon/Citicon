b5ef0aaefd1191199ec821157cc96546711b942c
johnpad53 <johnjosua.paderon@outlook.com>
2017-06-26 20:51:10 +0800

    Added: GetPurchaseOrderListBySelector.cs

---
+++ Citicon/DataProcess/GetPurchaseOrderListBySelector.cs
@@ -0,0 +1,43 @@
+﻿using Citicon.Data;
+using Citicon.DataManager;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Threading.Tasks;
+
+namespace Citicon.DataProcess
+{
+    internal sealed class GetPurchaseOrderListBySelector : DataProcessBase
+    {
+        public GetPurchaseOrderListBySelector(PurchaseOrderSelector selector)
+        {
+            Selector = selector;
+        }
+
+        private PurchaseOrderSelector Selector;
+
+        private MySqlCommand CreateCommand(MySqlConnection connection)
+        {
+            var command = Utility.CreateProcedureCommand("GetPurchaseOrderListBySelector", connection);
+            command.Parameters.AddWithValue("@_Selector", Selector.ToString());
+
+            return command;
+        }
+
+        private async Task<PurchaseOrder> FromReaderAsync(DbDataReader reader)
+        {
+            return new PurchaseOrder()
+            {
+                Id = reader.GetUInt64("Id"),
+                MaximumCumulativePricePerCubicMeter = reader.GetDecimal("MaximumCumulativePricePerCubicMeter"),
+                Number = reader.GetString("Number"),
+                Project = await ProjectManager.GetByIdAsync(reader.GetUInt64("ProjectId"))
+            };
+        }
+
+        public Task<IEnumerable<PurchaseOrder>> ExecuteAsync()
+        {
+            return ProcessUtility.HandleReadingIEnumerableAsync(CreateCommand, FromReaderAsync);
+        }
+    }
+}
