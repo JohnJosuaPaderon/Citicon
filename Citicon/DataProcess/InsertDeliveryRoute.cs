2b303c0913f3704318a2959a89053110babd0f92
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-06 11:51:52 +0800

    Added: InserDeliveryRoute.cs; UpdateDeliveryRoute.cs

---
+++ Citicon/DataProcess/InsertDeliveryRoute.cs
@@ -0,0 +1,51 @@
+﻿using Citicon.Data;
+using MySql.Data.MySqlClient;
+using System;
+using System.Threading.Tasks;
+
+namespace Citicon.DataProcess
+{
+    internal sealed class InsertDeliveryRoute : DataProcessBase
+    {
+        public InsertDeliveryRoute(DeliveryRoute route)
+        {
+            Route = route ?? throw new ArgumentNullException(nameof(route));
+        }
+
+        private DeliveryRoute Route;
+
+        private MySqlCommand CreateCommand(MySqlConnection connection, MySqlTransaction transaction)
+        {
+            var command = Utility.CreateProcedureCommand("InsertDeliveryRoute", connection, transaction);
+            command.Parameters.Add(Utility.CreateOutParameter("@_Id"));
+            command.Parameters.AddWithValue("@_Name", Route.Name);
+            command.Parameters.AddWithValue("@_StartPoint", Route.StartPoint);
+            command.Parameters.AddWithValue("@_EndPoint", Route.EndPoint);
+            command.Parameters.AddWithValue("@_Rate", Route.Rate);
+
+            return command;
+        }
+
+        public async Task<DeliveryRoute> ExecuteAsync(MySqlConnection connection, MySqlTransaction transaction)
+        {
+            using (var command = CreateCommand(connection, transaction))
+            {
+                if (await command.ExecuteNonQueryAsync() == 1)
+                {
+                    Route.Id = Convert.ToInt64(command.Parameters["@_Id"].Value);
+                }
+                else
+                {
+                    Route = null;
+                }
+
+                return Route;
+            }
+        }
+
+        public Task<DeliveryRoute> ExecuteAsync()
+        {
+            return ProcessUtility.HandleExecuteAsync(ExecuteAsync);
+        }
+    }
+}
---
+++ Citicon/DataProcess/UpdateDeliveryRoute.cs
@@ -0,0 +1,47 @@
+﻿using Citicon.Data;
+using MySql.Data.MySqlClient;
+using System;
+using System.Threading.Tasks;
+
+namespace Citicon.DataProcess
+{
+    internal sealed class UpdateDeliveryRoute : DataProcessBase
+    {
+        public UpdateDeliveryRoute(DeliveryRoute route)
+        {
+            Route = route ?? throw new ArgumentNullException(nameof(route));
+        }
+
+        private DeliveryRoute Route;
+
+        private MySqlCommand CreateCommand(MySqlConnection connection, MySqlTransaction transaction)
+        {
+            var command = Utility.CreateProcedureCommand("UpdateDeliveryRoute", connection, transaction);
+            command.Parameters.AddWithValue("@_Id", Route.Id);
+            command.Parameters.AddWithValue("@_Name", Route.Name);
+            command.Parameters.AddWithValue("@_StartPoint", Route.StartPoint);
+            command.Parameters.AddWithValue("@_EndPoint", Route.EndPoint);
+            command.Parameters.AddWithValue("@_Rate", Route.Rate);
+
+            return command;
+        }
+
+        public async Task<DeliveryRoute> ExecuteAsync(MySqlConnection connection, MySqlTransaction transaction)
+        {
+            using (var command = CreateCommand(connection, transaction))
+            {
+                if (await command.ExecuteNonQueryAsync() != 1)
+                {
+                    Route = null;
+                }
+
+                return Route;
+            }
+        }
+
+        public Task<DeliveryRoute> ExecuteAsync()
+        {
+            return ProcessUtility.HandleExecuteAsync(ExecuteAsync);
+        }
+    }
+}
