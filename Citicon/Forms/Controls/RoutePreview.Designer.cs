f7dfc78185544c76dbf10013a653e39bfd1ef736
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-06 11:52:19 +0800

    Added: RoutePreview.cs; RoutePreview.Designer.cs; RoutePreview.resx

---
+++ Citicon/Forms/Controls/RoutePreview.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Citicon.Forms.Controls
+{
+    partial class RoutePreview
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.FromTextBox = new System.Windows.Forms.TextBox();
+            this.FromLabel = new System.Windows.Forms.Label();
+            this.ToTextBox = new System.Windows.Forms.TextBox();
+            this.ToLabel = new System.Windows.Forms.Label();
+            this.RateTextBox = new System.Windows.Forms.TextBox();
+            this.RateLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Location = new System.Drawing.Point(4, 4);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(43, 17);
+            this.NameLabel.TabIndex = 0;
+            this.NameLabel.Text = "Name";
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.NameTextBox.Location = new System.Drawing.Point(7, 24);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(294, 25);
+            this.NameTextBox.TabIndex = 1;
+            // 
+            // FromTextBox
+            // 
+            this.FromTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.FromTextBox.Location = new System.Drawing.Point(7, 72);
+            this.FromTextBox.Name = "FromTextBox";
+            this.FromTextBox.Size = new System.Drawing.Size(294, 25);
+            this.FromTextBox.TabIndex = 3;
+            // 
+            // FromLabel
+            // 
+            this.FromLabel.AutoSize = true;
+            this.FromLabel.Location = new System.Drawing.Point(4, 52);
+            this.FromLabel.Name = "FromLabel";
+            this.FromLabel.Size = new System.Drawing.Size(38, 17);
+            this.FromLabel.TabIndex = 2;
+            this.FromLabel.Text = "From";
+            // 
+            // ToTextBox
+            // 
+            this.ToTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ToTextBox.Location = new System.Drawing.Point(7, 120);
+            this.ToTextBox.Name = "ToTextBox";
+            this.ToTextBox.Size = new System.Drawing.Size(294, 25);
+            this.ToTextBox.TabIndex = 5;
+            // 
+            // ToLabel
+            // 
+            this.ToLabel.AutoSize = true;
+            this.ToLabel.Location = new System.Drawing.Point(4, 100);
+            this.ToLabel.Name = "ToLabel";
+            this.ToLabel.Size = new System.Drawing.Size(22, 17);
+            this.ToLabel.TabIndex = 4;
+            this.ToLabel.Text = "To";
+            // 
+            // RateTextBox
+            // 
+            this.RateTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.RateTextBox.Location = new System.Drawing.Point(7, 168);
+            this.RateTextBox.Name = "RateTextBox";
+            this.RateTextBox.Size = new System.Drawing.Size(294, 25);
+            this.RateTextBox.TabIndex = 7;
+            // 
+            // RateLabel
+            // 
+            this.RateLabel.AutoSize = true;
+            this.RateLabel.Location = new System.Drawing.Point(4, 148);
+            this.RateLabel.Name = "RateLabel";
+            this.RateLabel.Size = new System.Drawing.Size(34, 17);
+            this.RateLabel.TabIndex = 6;
+            this.RateLabel.Text = "Rate";
+            // 
+            // RoutePreview
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.RateTextBox);
+            this.Controls.Add(this.RateLabel);
+            this.Controls.Add(this.ToTextBox);
+            this.Controls.Add(this.ToLabel);
+            this.Controls.Add(this.FromTextBox);
+            this.Controls.Add(this.FromLabel);
+            this.Controls.Add(this.NameTextBox);
+            this.Controls.Add(this.NameLabel);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Red;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "RoutePreview";
+            this.Size = new System.Drawing.Size(304, 199);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.TextBox FromTextBox;
+        private System.Windows.Forms.Label FromLabel;
+        private System.Windows.Forms.TextBox ToTextBox;
+        private System.Windows.Forms.Label ToLabel;
+        private System.Windows.Forms.TextBox RateTextBox;
+        private System.Windows.Forms.Label RateLabel;
+    }
+}
---
+++ Citicon/Forms/Controls/RoutePreview.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows.Forms;
+using Citicon.Data;
+
+namespace Citicon.Forms.Controls
+{
+    public partial class RoutePreview : UserControl
+    {
+        public RoutePreview()
+        {
+            InitializeComponent();
+        }
+
+        private DeliveryRoute _Route;
+
+        public DeliveryRoute Route
+        {
+            get { return _Route; }
+            set
+            {
+                if (_Route != value)
+                {
+                    _Route = value;
+                    OnRouteChanged();
+                }
+            }
+        }
+
+        private void OnRouteChanged()
+        {
+            NameTextBox.Text = Route?.Name;
+            FromTextBox.Text = Route?.StartPoint;
+            ToTextBox.Text = Route?.EndPoint;
+            RateTextBox.Text = Route?.Rate.ToString("#,##0.00");
+        }
+    }
+}
