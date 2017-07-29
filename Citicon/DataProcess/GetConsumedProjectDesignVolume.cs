a5072b63249a8d0ca0a2fef597afe324beb4e054
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-29 08:47:21 +0800

    Added: GetConsumedProjectDesignVolume.cs;

---
+++ Citicon/DataProcess/GetConsumedProjectDesignVolume.cs
@@ -0,0 +1,33 @@
+﻿using Citicon.Data;
+using MySql.Data.MySqlClient;
+using System;
+using System.Threading.Tasks;
+
+namespace Citicon.DataProcess
+{
+    internal sealed class GetConsumedProjectDesignVolume : DataProcessBase
+    {
+        public GetConsumedProjectDesignVolume(ProjectDesign projectDesign)
+        {
+            ProjectDesign = projectDesign ?? throw new ArgumentNullException(nameof(projectDesign));
+        }
+
+        private ProjectDesign ProjectDesign;
+
+        private MySqlCommand CreateCommand(MySqlConnection connection)
+        {
+            var command = new MySqlCommand()
+            {
+                Connection = connection,
+                CommandText = $"SELECT GetConsumedProjectDesignVolume({ProjectDesign.Id})"
+            };
+
+            return command;
+        }
+
+        public Task<decimal> ExecuteAsync()
+        {
+            return ProcessUtility.HandleExecuteScalarAsync(CreateCommand, Convert.ToDecimal);
+        }
+    }
+}
