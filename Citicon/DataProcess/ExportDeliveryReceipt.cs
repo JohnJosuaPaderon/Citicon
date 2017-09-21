c5767c8592d7169ffadaa2f50f4fb69704de84a1
johnpad53 <johnjosua.paderon@outlook.com>
2017-09-22 07:41:11 +0800

    Updated: ExportDeliveryReceipt.cs;

--- Citicon/DataProcess/ExportDeliveryReceipt.cs
+++ Citicon/DataProcess/ExportDeliveryReceipt.cs
@@ -57,16 +57,17 @@ namespace Citicon.DataProcess
                 Worksheet.Cells[12, 6] = Delivery.Load;
                 Worksheet.Cells[12, 8] = Delivery.TransitMixer?.PhysicalNumber;
                 Worksheet.Cells[14, 2] = Delivery.Project?.Name;
-                Worksheet.Cells[15, 6] = Delivery.Driver?.ToString();
+                Worksheet.Cells[15, 6] = Delivery.Driver?.FullName;
                 Worksheet.Cells[17, 1] = Delivery.Project?.Location;
                 Worksheet.Cells[21, 2] = Delivery.ProjectDesign?.Strength?.ToString();
                 Worksheet.Cells[21, 4] = Delivery.ProjectDesign?.Psi.ToString("###0");
-                Worksheet.Cells[22, 8] = Delivery.ProjectDesign?.PricePerCubicMeter.ToString("#,##0.00");
+                Worksheet.Cells[22, 8] = Delivery.Volume.ToString("#,##0.0");
                 Worksheet.Cells[23, 2] = Delivery.CumulativeVolume.ToString("#,##0.0");
                 Worksheet.Cells[24, 4] = Delivery.ProjectDesign?.Aggregate?.ToString();
                 Worksheet.Cells[24, 8] = Delivery.MaxSlump;
                 Worksheet.Cells[32, 2] = Delivery.Admixture;
                 Worksheet.Cells[32, 4] = Delivery.AdmixtureQuantity;
+                Worksheet.Cells[37, 1] = Delivery.PlantLeave?.ToString("HHmm");
 
                 if (!Directory.Exists(SaveLocation))
                 {
