c45756ce934a678f3d143ada5e08bfcdb01e55ab
johnpad53 <johnjosua.paderon@outlook.com>
2017-11-25 08:18:30 +0800

    Added: GenerateLatestIssuanceSlipNumber.cs; GetLatestStockByItem.cs;

---
+++ Citicon/DataProcess/GenerateLatestIssuanceSlipNumber.cs
@@ -0,0 +1,25 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Threading.Tasks;
+
+namespace Citicon.DataProcess
+{
+    internal sealed class GenerateLatestIssuanceSlipNumber : DataProcessBase
+    {
+        private MySqlCommand CreateCommand(MySqlConnection connection)
+        {
+            var command = new MySqlCommand()
+            {
+                Connection = connection,
+                CommandText = string.Format("SELECT GenerateLatestIssuanceSlipNumber();")
+            };
+
+            return command;
+        }
+
+        public Task<string> ExecuteAsync()
+        {
+            return ProcessUtility.HandleExecuteScalarAsync(CreateCommand, Convert.ToString);
+        }
+    }
+}
---
+++ Citicon/DataProcess/GetLatestStockByItem.cs
@@ -0,0 +1,43 @@
+﻿using Citicon.Data;
+using Citicon.DataManager;
+using MySql.Data.MySqlClient;
+using System.Data.Common;
+using System.Threading.Tasks;
+
+namespace Citicon.DataProcess
+{
+    internal sealed class GetLatestStockByItem : DataProcessBase
+    {
+        public GetLatestStockByItem(Item item)
+        {
+            Item = item;
+        }
+
+        private Item Item;
+
+        private MySqlCommand CreateCommand(MySqlConnection connection)
+        {
+            var command = Utility.CreateProcedureCommand("GetLatestStockByItem", connection);
+            command.Parameters.Add("@_ItemId", Item.Id);
+
+            return command;
+        }
+
+        private async Task<Stock> FromReaderAsync(DbDataReader reader)
+        {
+            return new Stock()
+            {
+                AcceptedBy = reader.GetString("AcceptedBy"),
+                AddedStockValue = reader.GetDecimal("AddedStockValue"),
+                Branch = BranchManager.GetById(reader.GetUInt64("BranchId")),
+                UnitPrice = reader.GetDecimal("UnitPrice"),
+                DeliveryDate = reader.GetDateTime("DeliveryDate")
+            };
+        }
+        
+        public Task<Stock> ExecuteAsync()
+        {
+            return ProcessUtility.HandleReadingAsync(CreateCommand, FromReaderAsync);
+        }
+    }
+}
