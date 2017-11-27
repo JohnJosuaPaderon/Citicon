1747e34f9e5bd9946a34489e2410034687664434
johnpad53 <johnjosua.paderon@outlook.com>
2017-11-27 08:28:28 +0800

    Updated: TimeLog.cs;

--- Citicon/Data/TimeLog.cs
+++ Citicon/Data/TimeLog.cs
@@ -8,6 +8,8 @@ namespace Citicon.Data
         public EmployeeLogin EmployeeLogin { get; set; }
         public DateTime? Login { get; set; }
         public DateTime? Logout { get; set; }
+        public bool LoginEdited { get; set; }
+        public bool LogoutEdited { get; set; }
         public TimeLogType Type { get; set; }
         public TimeSpan? Difference => Logout - Login;
 
