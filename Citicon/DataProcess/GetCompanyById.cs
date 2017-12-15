c11bbb5096cb6d5ef93240e5f8a4cb1b3fbe6127
johnpad53 <johnjosua.paderon@outlook.com>
2017-12-15 13:42:23 +0800

    Added: GetCompanyById.cs; GetSupplierById.cs;

---
+++ Citicon/DataProcess/GetCompanyById.cs
@@ -0,0 +1,39 @@
+﻿using Citicon.Data;
+using MySql.Data.MySqlClient;
+using System.Data.Common;
+using System.Threading.Tasks;
+
+namespace Citicon.DataProcess
+{
+    internal sealed class GetCompanyById : DataProcessBase
+    {
+        public GetCompanyById(ulong companyId)
+        {
+            _CompanyId = companyId;
+        }
+
+        private readonly ulong _CompanyId;
+
+        private Company FromReader(DbDataReader reader)
+        {
+            return new Company
+            {
+                Id = _CompanyId,
+                Code = reader.GetString("Code"),
+                Description = reader.GetString("Description")
+            };
+        }
+
+        private MySqlCommand CreateCommand(MySqlConnection connection)
+        {
+            var command = Utility.CreateProcedureCommand("_companies_getbyid", connection);
+            command.Parameters.AddWithValue("@_CompanyId", _CompanyId);
+            return command;
+        }
+
+        public Task<Company> ExecuteAsync()
+        {
+            return ProcessUtility.HandleReadingAsync(CreateCommand, FromReader);
+        }
+    }
+}
---
+++ Citicon/DataProcess/GetSupplierById.cs
@@ -0,0 +1,42 @@
+﻿using Citicon.Data;
+using MySql.Data.MySqlClient;
+using System.Data.Common;
+using System.Threading.Tasks;
+
+namespace Citicon.DataProcess
+{
+    internal sealed class GetSupplierById : DataProcessBase
+    {
+        public GetSupplierById(ulong supplierId)
+        {
+            _SupplierId = supplierId;
+        }
+
+        private readonly ulong _SupplierId;
+
+        private MySqlCommand CreateCommand(MySqlConnection connection)
+        {
+            var command = Utility.CreateProcedureCommand("_suppliers_getbyid", connection);
+            command.Parameters.AddWithValue("@_SupplierId", _SupplierId);
+
+            return command;
+        }
+
+        private Supplier FromReader(DbDataReader reader)
+        {
+            return new Supplier
+            {
+                Id = _SupplierId,
+                Address = reader.GetString("Address"),
+                Code = reader.GetString("Code"),
+                ContactNumber = reader.GetString("ContactNumber"),
+                Description = reader.GetString("Description")
+            };
+        }
+
+        public Task<Supplier> ExecuteAsync()
+        {
+            return ProcessUtility.HandleReadingAsync(CreateCommand, FromReader);
+        }
+    }
+}
