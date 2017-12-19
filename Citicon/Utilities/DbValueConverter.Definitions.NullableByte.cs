6a636b02da24dcaaec03396834fd0416836d29af
johnpad53 <johnjosua.paderon@outlook.com>
2017-12-20 07:53:48 +0800

    Added class: DbValueConverter

---
+++ Citicon/Utilities/DbValueConverter.Definitions.NullableByte.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Citicon.Utilities
+{
+    partial class DbValueConverter
+    {
+        public static byte? ToNullableByte(object value)
+        {
+            return ConversionBase(value, ValueConverter.ToNullableByte);
+        }
+
+        public static byte? ToNullableByte(object value, IFormatProvider formatProvider)
+        {
+            return ConversionBase(value, formatProvider, ValueConverter.ToNullableByte);
+        }
+    }
+}
