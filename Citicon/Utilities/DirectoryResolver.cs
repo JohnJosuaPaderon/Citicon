a2cb4f26ca8a1f58d3448d48e8e2fe2ce750b6b0
johnpad53 <johnjosua.paderon@outlook.com>
2017-12-26 19:15:23 +0800

    Added: DirectoryResolver.cs;

---
+++ Citicon/Utilities/DirectoryResolver.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+namespace Citicon.Utilities
+{
+    public static class DirectoryResolver
+    {
+        public static void Resolve(string directory)
+        {
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+        }
+    }
+}
