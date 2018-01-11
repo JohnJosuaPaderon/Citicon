72c9281ee6bb6ae046ec1bc9e2e52cee6a2bcb87
johnpad53 <johnjosua.paderon@outlook.com>
2018-01-12 06:18:48 +0800

    Added: CancelCheque.cs; VoidChequeExists.cs;
    Removed: ExportInventoryIssuanceSlip.cs;

---
+++ Citicon/DataProcess/CancelCheque.cs
@@ -0,0 +1,52 @@
+﻿using Citicon.Data;
+using Citicon.Extensions;
+using MySql.Data.MySqlClient;
+using System;
+using System.Threading.Tasks;
+
+namespace Citicon.DataProcess
+{
+    internal sealed class CancelCheque : DataProcessBase
+    {
+        public CancelCheque(VoidCheque voidCheque)
+        {
+            _VoidCheque = voidCheque;
+        }
+
+        private readonly VoidCheque _VoidCheque;
+
+        private MySqlCommand CreateCommand(MySqlConnection connection, MySqlTransaction transaction)
+        {
+            return Utility.CreateProcedureCommand(nameof(CancelCheque), connection, transaction)
+                .AddOutParameter("@_Id")
+                .AddInParameter("@_ChequeNumber", _VoidCheque.ChequeNumber)
+                .AddInParameter("@_VoidDate", DateTime.Now)
+                .AddInParameter("@_VoidBy", User.CurrentUser?.Id)
+                .AddInParameter("@_BankAccountId", _VoidCheque.BankAccount?.Id)
+                .AddInParameter("@_Remarks", _VoidCheque.Remarks);
+        }
+        
+        public async Task<VoidCheque> ExecuteAsync(MySqlConnection connection, MySqlTransaction transaction)
+        {
+            using (var command = CreateCommand(connection, transaction))
+            {
+                var affectedRows = await command.ExecuteNonQueryAsync();
+
+                if (affectedRows > 0)
+                {
+                    _VoidCheque.Id = command.Parameters.GetUInt64("@_Id");
+                    return _VoidCheque;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        public Task<VoidCheque> ExecuteAsync()
+        {
+            return ProcessUtility.HandleExecuteAsync(ExecuteAsync);
+        }
+    }
+}
---
+++ Citicon/DataProcess/VoidChequeExists.cs
@@ -0,0 +1,29 @@
+﻿using Citicon.Data;
+using Citicon.Utilities;
+using MySql.Data.MySqlClient;
+using System.Threading.Tasks;
+
+namespace Citicon.DataProcess
+{
+    internal sealed class VoidChequeExists : DataProcessBase
+    {
+        public VoidChequeExists(string chequeNumber, BankAccount bankAccount)
+        {
+            _ChequeNumber = chequeNumber;
+            _BankAccount = bankAccount;
+        }
+
+        private readonly string _ChequeNumber;
+        private readonly BankAccount _BankAccount;
+
+        private MySqlCommand CreateCommand(MySqlConnection connection)
+        {
+            return new MySqlCommand($"SELECT IsVoidChequeExists('{_ChequeNumber}', {_BankAccount?.Id})", connection);
+        }
+
+        public Task<bool> ExecuteAsync()
+        {
+            return ProcessUtility.HandleExecuteScalarAsync(CreateCommand, DbValueConverter.ToBoolean);
+        }
+    }
+}
