c6af25a25378f1012d92ef74eb54a97e0f8ad8ca
johnpad53 <johnjosua.paderon@outlook.com>
2018-01-12 06:17:45 +0800

    Added: VoidCheque.cs;

---
+++ Citicon/Data/VoidCheque.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Citicon.Data
+{
+    public class VoidCheque : EntityBase<ulong>
+    {
+        public string ChequeNumber { get; set; }
+        public BankAccount BankAccount { get; set; }
+        public DateTime ChequeDate { get; set; }
+        public DateTime VoidDate { get; set; }
+        public User VoidBy { get; set; }
+        public string Remarks { get; set; }
+    }
+}
