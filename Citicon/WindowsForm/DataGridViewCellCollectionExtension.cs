a43ccdab8786a6f713df219cf17f86e33ac367b3
johnpad53 <johnjosua.paderon@outlook.com>
2018-01-18 14:14:54 +0800

    Added: DataGridViewCellCollectionExtension.cs; DataGridViewRowExtension.cs;

---
+++ Citicon/WindowsForm/DataGridViewCellCollectionExtension.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Forms;
+
+namespace Citicon.WindowsForm
+{
+    public static class DataGridViewCellCollectionExtension
+    {
+        private static DataGridViewCellCollection AddTextBoxBase(this DataGridViewCellCollection instance, object value)
+        {
+            var cell = new DataGridViewTextBoxCell
+            {
+                Value = value
+            };
+
+            instance.Add(cell);
+            return instance;
+        }
+
+        public static DataGridViewCellCollection AddTextBox(this DataGridViewCellCollection instance, object value)
+        {
+            return instance.AddTextBoxBase(value);
+        }
+    }
+}
---
+++ Citicon/WindowsForm/DataGridViewRowExtension.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Forms;
+
+namespace Citicon.WindowsForm
+{
+    public static class DataGridViewRowExtension
+    {
+        public static DataGridViewRow SetHeight(this DataGridViewRow instance, int height)
+        {
+            instance.Height = height;
+            return instance;
+        }
+
+        public static DataGridViewRow AddTextBoxCell(this DataGridViewRow instance, object value)
+        {
+            instance.Cells.AddTextBox(value);
+            return instance;
+        }
+    }
+}
