cabef3ab4d94daa65295eb87a7d372c41662cd0b
johnpad53 <johnjosua.paderon@outlook.com>
2018-01-18 14:12:20 +0800

    Added: OutgoingStockManager.cs;

---
+++ Citicon/DataManager/OutgoingStockManager.cs
@@ -0,0 +1,30 @@
+﻿using Citicon.Data;
+using Citicon.DataProcess;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Citicon.DataManager
+{
+    public static class OutgoingStockManager
+    {
+        public static async Task<IEnumerable<OutgoingStock>> GetFilteredListAsync(GetFilteredOutgoingStockListOption option)
+        {
+            using (var process = new GetFilteredOutgoingStockList(option))
+            {
+                return await process.ExecuteAsync();
+            }
+        }
+
+        public static async Task ExportReportAsync(IEnumerable<OutgoingStock> outgoingStocks)
+        {
+            if (outgoingStocks != null && outgoingStocks.Any())
+            {
+                using (var process = new ExportOutgoingStockReport(outgoingStocks))
+                {
+                    await process.ExecuteAsync();
+                }
+            }
+        }
+    }
+}
