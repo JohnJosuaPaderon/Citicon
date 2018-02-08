44bbe4e8e39449d106c59605aa3e1a5385068e35
johnpad53 <johnjosua.paderon@outlook.com>
2018-02-08 12:00:33 +0800

    Added functions related to Windows Forms controls

--- Citicon/Forms/DataLinks/DataGridViewItemSource.cs
+++ Citicon/Forms/DataLinks/DataGridViewItemSource.cs
@@ -5,7 +5,7 @@ using System.Windows.Forms;
 
 namespace Citicon.Forms.DataLinks
 {
-    public class DataGridViewItemSource<T> : IEnumerable<T>
+    public class DataGridViewItemSource<T> : IEnumerable<T>, IDisposable
         where T : class
     {
         public DataGridViewItemSource(DataGridView dataGridView, Func<T, DataGridViewRow> addRow, string itemColumnName)
@@ -14,9 +14,25 @@ namespace Citicon.Forms.DataLinks
             _DataGridView = dataGridView ?? throw new ArgumentNullException(nameof(dataGridView));
             _AddRow = addRow ?? throw new ArgumentNullException(nameof(addRow));
             _ItemColumnName = itemColumnName ?? throw new ArgumentNullException(nameof(itemColumnName));
+
+            _DataGridView.SelectionChanged += SelectionChanged;
+        }
+
+        public DataGridViewItemSource(DataGridView dataGridView, Func<T, DataGridViewRow> addRow, Action<T> selectedItemChanged, string itemColumnName, Action<DataGridViewRow, T> refreshRow)
+        {
+            _Items = new List<T>();
+            _DataGridView = dataGridView ?? throw new ArgumentNullException(nameof(dataGridView));
+            _AddRow = addRow ?? throw new ArgumentNullException(nameof(addRow));
+            _ItemColumnName = itemColumnName ?? throw new ArgumentNullException(nameof(itemColumnName));
+            _SelectedItemChanged = selectedItemChanged;
+            _RefreshRow = refreshRow;
+
+            _DataGridView.SelectionChanged += SelectionChanged;
         }
 
         private readonly Func<T, DataGridViewRow> _AddRow;
+        private readonly Action<T> _SelectedItemChanged;
+        private readonly Action<DataGridViewRow, T> _RefreshRow;
         private readonly DataGridView _DataGridView;
         private readonly List<T> _Items;
         private readonly string _ItemColumnName;
@@ -27,7 +43,8 @@ namespace Citicon.Forms.DataLinks
             {
                 if (_DataGridView.SelectedRows.Count == 1)
                 {
-                    return _DataGridView.SelectedRows[0].Cells[_ItemColumnName].Value as T;
+                    var result = _DataGridView.SelectedRows[0].Cells[_ItemColumnName].Value as T;
+                    return result;
                 }
                 else
                 {
@@ -87,6 +104,26 @@ namespace Citicon.Forms.DataLinks
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
+        }
+
+        public void Dispose()
+        {
+            _DataGridView.SelectionChanged -= SelectionChanged;
+        }
+
+        private void SelectionChanged(object sender, EventArgs e)
+        {
+            _SelectedItemChanged?.Invoke(SelectedItem);
+        }
+
+        private void RepaintSelectedItem()
+        {
+            _DataGridView.Refresh();
+        }
+
+        public void RefreshSelected()
+        {
+            _RefreshRow(_DataGridView.SelectedRows[0], SelectedItem);
         }
     }
 }
---
+++ Citicon/Forms/DataLinks/TextBoxHelper.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Citicon.Forms.DataLinks
+{
+    public sealed class TextBoxHelper<TResult> : IDisposable
+    {
+        public static TextBoxHelper<decimal> CurrencyHelper(TextBox textBox, bool listenAccept = true, Action<decimal> accept = null)
+        {
+            return new TextBoxHelper<decimal>(textBox, listenAccept: listenAccept, accept: accept, format: FormatCurrency, parse: ParseCurrency);
+        }
+
+        private static string FormatCurrency(string text)
+        {
+            decimal.TryParse(text, out decimal result);
+            return result.ToString(Resources.Formats.Currency);
+        }
+
+        private static decimal ParseCurrency(string text)
+        {
+            decimal.TryParse(text.Replace(",", string.Empty), out decimal result);
+            return result;
+        }
+
+        public TextBoxHelper(TextBox textBox, bool listenAccept = false, Action<TResult> accept = null, Func<string, string> format = null, Func<string, TResult> parse = null)
+        {
+            _TextBox = textBox ?? throw new ArgumentNullException(nameof(textBox));
+            _ListenAccept = listenAccept;
+            _Format = format;
+            _Parse = parse;
+
+            if (_ListenAccept)
+            {
+                _TextBox.KeyDown += KeyDown;
+                _Accept = accept;
+            }
+
+            _TextBox.Leave += Leave;
+        }
+
+        private readonly TextBox _TextBox;
+        private readonly bool _ListenAccept;
+        private readonly Action<TResult> _Accept;
+        private readonly Func<string, TResult> _Parse;
+        private readonly Func<string, string> _Format;
+
+        public void Dispose()
+        {
+            if (_ListenAccept)
+            {
+                _TextBox.KeyDown += KeyDown;
+            }
+        }
+
+        private void KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Enter)
+            {
+                TryAccept();
+                TryFormat();
+            }
+        }
+
+        private void Leave(object sender, EventArgs e)
+        {
+            TryAccept();
+            TryFormat();
+        }
+
+        private void TryAccept()
+        {
+            if (_ListenAccept)
+            {
+                _Accept(_Parse(_TextBox.Text));
+            }
+        }
+
+        private void TryFormat()
+        {
+            _TextBox.Text = _Format?.Invoke(_TextBox.Text);
+        }
+    }
+}
