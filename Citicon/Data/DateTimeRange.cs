80c5c8620c467409d2072b132ff7fbc64a5a0e41
johnpad53 <johnjosua.paderon@outlook.com>
2018-02-15 11:37:54 +0800

    Implements equality comparison.

--- Citicon/Data/DateTimeRange.cs
+++ Citicon/Data/DateTimeRange.cs
@@ -19,6 +19,43 @@ namespace Citicon.Data
         public DateTime Begin { get; set; }
         public DateTime End { get; set; }
 
+
+        public static bool operator ==(DateTimeRange left, DateTimeRange right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(DateTimeRange left, DateTimeRange right)
+        {
+            return !(left == right);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (GetType() != obj.GetType()) return false;
+
+            if (obj is DateTimeRange value)
+            {
+                return (
+                    Equals(Begin, default(DateTime)) ||
+                    Equals(value.Begin, default(DateTime)) ||
+                    Equals(End, default(DateTime)) || 
+                    Equals(value.End, default(DateTime)))
+                    ? false : Equals(Begin, value.Begin) && Equals(End, value.End);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return Begin.GetHashCode();
+        }
+
         public override string ToString()
         {
             if (Begin.Date == End.Date)
